2af78fb37aed13a190e98825815657ad63c57ff2
STD KING <112249837+StdKing@users.noreply.github.com>
2023-05-31 12:06:00 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -25,7 +25,7 @@ def changeImageSize(maxWidth, maxHeight, image):
 def add_corners(im):
     bigsize = (im.size[0] * 1, im.size[1] * 1)
     mask = Image.new("L", bigsize, 0)
-    ImageDraw.Draw(mask).ellipse((0, 0) + bigsize, fill=255)
+    ImageDraw.Draw(mask).ellipse((0, 0) + bigsize, fill=1)
     mask = mask.resize(im.size, Image.ANTIALIAS)
     mask = ImageChops.darker(mask, im.split()[-1])
     im.putalpha(mask)
